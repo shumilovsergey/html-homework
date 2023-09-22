34a2e8e7b1692c015e9c2dc9189acc8f290da7fe
shumilov <wumilovsergey@gmail.com>
2023-09-22 14:34:19 +0300

    dashboard rdy

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,57 @@ function closeSidebar() {
         sidebarOpen = false;
     }
 }
+
+//--CHARTS--//
+
+var options = {
+    series: [{
+    data: [10, 8, 6, 4, 2]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350,
+    toolbar: {
+        show: false
+    },
+  },
+
+  colors: [
+    "#246dec",
+    "#cc3c43",
+    "#367952",
+    "#f5b74f",
+    "4f35a1"
+  ],
+
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: '40%', 
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+
+  legend: {
+    show: false,
+  },
+
+  xaxis: {
+    categories: ["Serega", "Ebet", "Dashbord", "Ochen", "Jestko"],
+  },
+
+  yaxis: {
+    title: {
+        text: "Count"
+    }
+  }
+};
+
+  var barChart = new ApexCharts(document.querySelector("#bar-chart"), options);
+  barChart.render();
+
+
